cb5e3176534cc29f5bb171d02e5a5438baceac3f
Aimar Aareleid <aimar@snapit.ee>
2022-03-10 10:27:07 +0100

    update

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
   className: string;
   active: string;
   handleMouseEnter: (id: string) => void;
   handleMouseLeave: () => void;
-  image: string;
   urlTo: string;
   countryLabel: string;
   countryId: string;
@@ -17,7 +17,6 @@ export default function Section({
   active,
   handleMouseEnter,
   handleMouseLeave,
-  image,
   urlTo,
   countryLabel,
   countryId,
@@ -34,7 +33,14 @@ export default function Section({
           active !== countryId && active !== '' ? 'unactive' : null
         }`}
       >
-        <img src={image} className="logo" />
+        <Image
+          layout="fixed"
+          width="134"
+          height="50"
+          src="https://static.wixstatic.com/media/7acf0b_4c9560a517ba4ef285ce8827cafff07e~mv2.png/v1/fill/w_134,h_50,al_c,usm_0.66_1.00_0.01,enc_auto/7acf0b_4c9560a517ba4ef285ce8827cafff07e~mv2.png"
+          className="logo"
+          alt={countryLabel}
+        />
         <Link href={urlTo} passHref>
           <a href="#" className="btn">
             {buttonText}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,6 @@ export default function Home() {
           active={active}
           handleMouseEnter={handleMouseEnter}
           handleMouseLeave={handleMouseLeave}
-          image="https://static.wixstatic.com/media/7acf0b_4c9560a517ba4ef285ce8827cafff07e~mv2.png/v1/fill/w_134,h_50,al_c,usm_0.66_1.00_0.01,enc_auto/7acf0b_4c9560a517ba4ef285ce8827cafff07e~mv2.png"
           urlTo="http://worth.systems"
           countryLabel="The Netherlands"
           countryId="nl"
@@ -30,7 +29,6 @@ export default function Home() {
           active={active}
           handleMouseEnter={handleMouseEnter}
           handleMouseLeave={handleMouseLeave}
-          image="https://static.wixstatic.com/media/7acf0b_4c9560a517ba4ef285ce8827cafff07e~mv2.png/v1/fill/w_134,h_50,al_c,usm_0.66_1.00_0.01,enc_auto/7acf0b_4c9560a517ba4ef285ce8827cafff07e~mv2.png"
           urlTo="http://worth.digital"
           countryLabel="England"
           countryId="eng"
